6953668652abdce04b68936ff77bc53ee786ca65
Betselot Tadele <87697032+betsigit@users.noreply.github.com>
2023-12-13 10:04:14 +0300

    Time: 44 ms (45.90%), Space: 16.2 MB (52.59%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,25 +1,31 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        # Set to keep track of visited numbers
         visit = set()
-        
+
+        # Continue the loop until n is already visited
         while n not in visit:
+            # Add the current number to the set of visited numbers
             visit.add(n)
-            n= self.sumOfSquares(n)
             
+            # Calculate the sum of squares of digits
+            n = self.sumOfSquares(n)
+            
+            # If the sum of squares is 1, then the number is happy
             if n == 1:
                 return True
-            
-            
+
+        # If the loop ends and n is not 1, then it's not a happy number
         return False
-    
-    def sumOfSquares(self,n: int) ->int:
-        output =0
-        
+
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+
+        # Calculate the sum of squares of each digit in the number
         while n:
-            digit =n% 10
-            digit =digit ** 2
+            digit = n % 10
+            digit = digit ** 2
             output += digit
-            n= n//10
-            
+            n = n // 10
+
         return output
-        
